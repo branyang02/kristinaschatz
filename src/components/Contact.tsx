ceca13d31ce744568227a4c9ea6217f166c4cd30
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2024-05-10 06:52:51 +0800

    fixed bug where a tag changes color in project page.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,8 @@
 import '../styles/Contact.css';
 
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'; // Social media icons
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'; // Email icon
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // If using Font Awesome
-import React from 'react';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Contact = () => {
   return (
